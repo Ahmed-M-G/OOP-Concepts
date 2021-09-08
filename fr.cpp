b7b16e5573a7be0b74d9935cefd37542bad76709
Ahmed Mohamed Gaber <68587770+Ahmed-M-G@users.noreply.github.com>
2021-09-08 20:41:05 +0200

    Create fr.cpp

---
+++ fr.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<string>
+#include<math.h>
+using namespace std;
+/*class point
+{
+	int x,y;
+public:
+	void set()
+	{
+		cout<<"x :"; cin>>x;
+		cout<<"y :"; cin>>y;
+	}
+	void dis()
+	{
+		cout<<"x :"<<x<<endl;
+		cout<<"y :"<<y<<endl;
+	}
+	float dist(point A)
+	{
+		float d;
+		d=sqrt(pow((double)x-(A.x),2)+pow((double)(y-A.y),2));
+		return d;
+	}
+};*/
+//----------------------------------------------------
+/*class comp
+{
+	int a,b;
+public:
+	void set()
+	{
+		cout<<"a :"; cin>>a;
+		cout<<"b :"; cin>>b;
+	}
+	void dis()
+	{
+		cout<<"a :"<<a<<endl;
+		cout<<"b :"<<b<<endl;
+	}
+	friend comp sum(comp A,comp B)
+	{
+		comp C;
+		C.a=A.a+B.a;
+		C.b=A.b+B.b;
+		return C;
+	}
+};*/
+void main()
+{
+	/*point P,h;
+	P.set();
+	h.set();
+	P.dis();
+	cout<<"dist :"<<P.dist(h)<<endl;*/
+	//-------------------------------------------
+
+	/*comp X,Y,W;
+	X.set();
+	Y.set();
+	W=sum(X,Y);
+	W.dis();*/
+	
+	system("pause");
+}
