5d72a69d03d54eab6b516de743f443e9c15ed7da
Ahmed Mohamed Gaber <68587770+Ahmed-M-G@users.noreply.github.com>
2021-09-08 20:43:49 +0200

    Create struct.cpp

---
+++ struct.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+using namespace std;
+/*struct emp
+{
+    int empno;
+	string fname;
+	string lname;
+	float salary;
+	float bouns ;
+};*/
+//--------------------------------------------	
+struct student
+{
+	int sno;
+	string name;
+	float gpa;
+	float marks[3];
+};
+void main()
+{
+	/*emp e1,e2;
+	cout<<" enter emp 1 \n";
+	cout<<"empno = ";  cin>>e1.empno;
+	cout<<"fname = ";  cin>>e1.fname;
+	cout<<"lname = ";  cin>>e1.lname;
+	cout<<"salary = ";  cin>>e1.salary;
+	cout<<"bouns = ";  cin>>e1.bouns;
+	cout<<" write emp 1 \n";
+	cout<<"empno = "<<e1.empno<<endl;
+	cout<<"fname = "<<e1.fname<<endl;
+	cout<<"lname = "<<e1.lname<<endl;
+	cout<<"salary = "<<e1.salary<<endl;
+	cout<<"bouns = "<<e1.bouns<<endl;
+	//--------------------------------------------
+	e2=e1;
+	cout<<e2.bouns<<endl;
+	//--------------------------------------------
+	if(e2.bouns == e1.bouns)
+		cout<<"error \n";*/
+	//--------------------------------------------
+
+	/*student s1;
+	cout<<"sno = "; cin>>s1.sno;
+	cout<<"name = "; cin>>s1.name;
+	cout<<"gpa = "; cin>>s1.gpa;
+	for(int i=0;i<3;i++)
+	{
+		cout<<"marks = "; cin>>s1.marks[i];
+	}*/
+
+	//-----------------------------------------------
+	/*int n;
+	student x[100];
+	cout<<"n= "; cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cout<<"x["<<i+1<<"] sno ="; cin>>x[i].sno;
+		cout<<"x["<<i+1<<"] gpa ="; cin>>x[i].gpa;
+		cout<<"x["<<i+1<<"] name ="; cin>>x[i].name;
+	}*/
+
+	//------------------------------------------------
+
+
+
+
+
+
+	system("pause");
+}
