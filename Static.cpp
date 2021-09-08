bb1442eac55b26d0f5bb00f63c4140e6674901f5
Ahmed Mohamed Gaber <68587770+Ahmed-M-G@users.noreply.github.com>
2021-09-08 20:43:45 +0200

    Create Static.cpp

---
+++ Static.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<math.h>
+#include<string>
+using namespace std;
+/*class student
+{
+	string name,add;
+	int age,id;
+	float mark[3];
+	static int r;
+public:
+	void set()
+	{
+		cout<<"name";cin>>name;
+		cout<<"add"; cin>>add;
+		cout<<"age"; cin>>age;
+		for(int i=0;i<3;i++)
+		{cin>>mark[i];}
+		r++;
+		id=r;
+	}
+	void dis()
+	{
+		cout<<"name"<<name<<endl;
+		cout<<"add"<<add<<endl;
+		cout<<"age"<<age<<endl;
+		for(int i=0;i<3;i++)
+		{cout<<mark[i]<<endl;}
+		cout<<"id"<<id;
+	}
+	
+};
+int student::r=123456;*/
+//-----------------------------------------
+
+class A
+{
+
+};
+void main()
+{
+	/*student A,B;
+	A.set();
+	A.dis();
+	B.set();
+	B.dis();*/
+
+	//----------------------------------
+
+	
+	system("pause");
+}
